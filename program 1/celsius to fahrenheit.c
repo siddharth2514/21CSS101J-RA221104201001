57fac493c11c176c2e63ff1f942fadc9091c8a67
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-09-28 23:13:59 +0530

    Create celsius to fahrenheit.c

---
+++ program 1/celsius to fahrenheit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  float fahrenheit,celsius;
+  scanf("%f",&celsius);
+  printf("celsius=%f \n", celsius);
+  fahrenheit=1.8*celsius+32.0;
+  printf("fahrenheit=%f \n", fahrenheit);
+  return 0;
+}  
