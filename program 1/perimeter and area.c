0506aa930e53314085b2616d9903da43d68f6842
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-09-28 22:19:29 +0530

    Create perimeter and area.c

---
+++ program 1/perimeter and area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int p,q,area,perimeter;
+    p=4;
+    q=6;
+    area=p*q;
+    perimeter=2*(p+q);
+    printf("area=%d \n", area);
+    printf("perimeter=%d \n", perimeter);
+}    
