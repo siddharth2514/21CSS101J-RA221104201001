b1fc41a47ea8fc327890cbb5d36b5d42f7912677
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-10-05 15:07:14 +0530

    Create squaring a number inputted by the user.c

---
+++ program 1/squaring a number inputted by the user.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main() {
+  int number,square;
+  printf("Enter a number: ");
+  scanf("%d", &number);
+  square= number*number;
+  printf("The square of the number you entered is %d\n", square);
+}  
