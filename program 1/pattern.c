4c54f714f9a09b114b08cf33e86235a53288aa8a
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-10-07 10:37:34 +0530

    Create pattern.c

---
+++ program 1/pattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main() {
+  int i,j,k;
+    printf("\n\nOutput of the nested loop is :\n\n");
+    for(i = 0; i < 5; i++)
+    {
+        printf("\t\t\t\t");
+        for(j = 0; j < 5; j++)
+        printf("* ");
+      
+     printf("\n");
+    }      
+}
