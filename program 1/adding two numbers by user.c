bdcd5de68fca7524365b0da8c4e4f1d3835c7388
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-10-09 22:24:45 +0530

    Update adding two numbers by user.c

--- program 1/adding two numbers by user.c
+++ program 1/adding two numbers by user.c
@@ -2,6 +2,8 @@
 main() {
   int num1, num2, result;
   printf("Enter two numbers: ");
-  scanf("%d", &number, "%d", &number1);
-  result= number+ number1;
-  printf("The sum is," "%d");
+  scanf("%d", &num1 );
+  scanf("%d", &num2};
+  result= num1+ num2;
+  printf("The sum is, %d" , result);
+        }        
