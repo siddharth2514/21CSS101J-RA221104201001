7b4de9aa54ef730633dab4be417bd29dc2847d60
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-14 10:16:47 +0530

    Create 31) Average of two numbers.c

---
+++ program 1/31) Average of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main() {
+  int num1,num2,average
+  printf("Enter first number: ");
+  scanf("%d", &num1);
+  printf("Enter second number: ");
+  scanf("%d", &num2);
+  average=(num1+num2)/2;
+  printf("Average is "%d", average);
