b4708bcae186251635617e03a25e5594e66b5b7b
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-21 09:08:42 +0530

    Create 33) codetantra pass or fail.c

---
+++ program 1/33) codetantra pass or fail.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+  int marks, distinction marks = 75;
+  printf("Enter marks : ");
+  scanf("%d", &marks):
+  if(marks>distinction_marks) {
+    printf("User secured distinction.\n");
+  } else{
+    printf("User did not secure distinction.\n");
+  }    
