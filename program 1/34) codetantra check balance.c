37dfe7310f8a2a728ffc0fa47e000530cb1ed40c
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-21 09:15:57 +0530

    Create 34) codetantra check balance.c

---
+++ program 1/34) codetantra check balance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(){
+  int bal;
+  printf("Enter balance : ");
+  scanf("%d", &bl):
+  if(bal>=1000)
+  {
+    printf("Sufficient balance\n");
+  }
+  else
+  {
+    printf("Balance is low\n");
+  }
+}  
