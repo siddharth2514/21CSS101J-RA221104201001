5b4c39da67918e036d47df3cd68297212d350cbf
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-21 13:21:05 +0530

    Create 46) codetantra date format.c

---
+++ program 1/46) codetantra date format.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+main(){
+  int year,month,date;
+  printf("Enter DOB in yyyy.mm.dd format : ");
+  scanf("%d.%d.%d",&year,&month,&date);
+  printf("DOB is : %d.%d.%d\n",year,month,date);
