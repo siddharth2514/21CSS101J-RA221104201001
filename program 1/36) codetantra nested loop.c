f847656c14c8036ca1a7e5f09303917f90334241
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-21 09:31:22 +0530

    Create 36) codetantra nested loop.c

---
+++ program 1/36) codetantra nested loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+  int a, b, b;
+  printf("Enter any three numbers: ");
+  scanf("%d %d %d", &a,&b,&c);
+  if(a>b) {
+    if(a>c) {
+      printf("%d is greater than %d and %d\n", a,b,c);
+    }else if(c>a) {
+      printf("
