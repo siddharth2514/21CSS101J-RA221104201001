5a1e276a3c2d38b27c1be9055740e3bd42cc5c05
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-21 09:55:10 +0530

    Create 38) codetantra octal number.c

---
+++ program 1/38) codetantra octal number.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+main() {
+  int a;
+  printf("Enter a decimal integer : ",a);
+  scanf("%d", &a);
+ printf("Hexadecimal number : %x\n",a);
+  printf("Octal number : %o\n", a);
+}  
