d3532f7d16de5dd8995f56a01ad11690176a3cc4
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-25 02:56:45 +0530

    Create 78)factorial.c

---
+++ program 1/78)factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+  int number;
+  printf("printing factorial of the number : \n");
+  scanf("%d",&number);
+  int factorial = 1;
+  for(int i = 1 ; i <= number; i++)
+  {
+    factorial = factorial * i;
+  }
+    printf("factorial of the number is %d\n",factorial);
+  }
