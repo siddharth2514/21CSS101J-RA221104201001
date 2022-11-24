61d71351524a479a463b9af3fd6b0fc69ae8d460
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-25 02:18:22 +0530

    Create 68)calculating largest number.c

---
+++ program 1/68)calculating largest number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+  int a , b , c;
+  scanf("%d %d %d", &a , &b ,&c);
+  if(a>c)
+  printf("a = %4d\n" , a);
+  else 
+    printf("c = %4d\n", c);
+}
+else 
+{
+  if (b > c)
+    printf("b = %4d\n", b);
+  else
+    printf("c = %4d\n", c);
+}
+}
