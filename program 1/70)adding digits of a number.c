6d7be4486e39c9e367fd98d5391c39c769cfa100
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-25 02:22:47 +0530

    Create 70)adding digits of a number.c

---
+++ program 1/70)adding digits of a number.c
@@ -0,0 +1,16 @@
+#include < studio.h>
+main() 
+{
+int digit_1 , digit_2 , digit_3 , digit_4, digit_5 , sum , number , n; 
+scanf( "%d", &number); 
+n = number; 
+digit_1 = n % 10; 
+n = n / 10; 
+digit_2 = n % 10; 
+n = n / 10; 
+digit_4 = n %10; 
+n = n / 10; 
+digit_5 = n; 
+sum = digit_1 + digit_2 + digit_3 + digit_4 + digit_5; 
+printf("sum of digits = %d\n" , sum ) ; 
+}
