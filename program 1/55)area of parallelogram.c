8149558989c2f85003c05063dcd9fa412ca64175
siddharth2514 <113873485+siddharth2514@users.noreply.github.com>
2022-11-25 01:10:53 +0530

    Create 55)area of parallelogram.c

---
+++ program 1/55)area of parallelogram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ main()
+{
+  int base , height; 
+  printf("enter value of base : ",base); 
+  scanf("%d",&base);
+  printf("enter value of height : ",height);
+  scanf ("%d",&height);
+  int area = base * height;
+  printf("area of the parallelogram is : %d",area);
+}
